1c1254a12057f30f1a9ccc02ce964249367deb50
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2019-09-06 21:06:35 +0200

    json response

--- src/main/java/com/abstractdog/hive/web/tools/controller/ToolController.java
+++ src/main/java/com/abstractdog/hive/web/tools/controller/ToolController.java
@@ -3,7 +3,7 @@ package com.abstractdog.hive.web.tools.controller;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
-import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -28,19 +28,23 @@ public class ToolController {
   private String lipwigOutputPath;
 
   @RequestMapping(value = "/lipwig", method = RequestMethod.POST)
-  public String runLipwig(@RequestParam Map<String, Object> params) throws Exception {
-
-    File inputFile = File.createTempFile("lipwig-input", null);
-    File outputFile = File.createTempFile("lipwig-output", ".svg");
-
+  public Map<String, Object> runLipwig(@RequestParam Map<String, Object> params) throws Exception {
     String plan = params.get("plan").toString();
     System.out.println(plan);
 
-    Files.write(plan.getBytes(), inputFile);
+    boolean success = false;
+    StringBuilder sb = new StringBuilder();
+
+    File outputFile = File.createTempFile("lipwig-output", ".svg");
+    File finalOutputFile = new File(lipwigOutputFolder, outputFile.getName());
 
     try {
-      ProcessBuilder pb = new ProcessBuilder(lipwigPath, "-i", inputFile.getAbsolutePath(), "-o",
-          outputFile.getAbsolutePath());
+      File inputFile = File.createTempFile("lipwig-input", null);
+
+      Files.write(plan.getBytes(), inputFile);
+
+      ProcessBuilder pb =
+          new ProcessBuilder(lipwigPath, "-i", inputFile.getAbsolutePath(), "-o", outputFile.getAbsolutePath());
       pb.directory(new File(lipwigPath).getParentFile());
       System.out.println(pb.command());
 
@@ -50,21 +54,32 @@ public class ToolController {
         BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()))) {
         String line;
 
+        sb.append("ERROR OUT: \n");
         while ((line = error.readLine()) != null) {
-          System.out.println(line);
+          sb.append(line + "\n");
         }
+        sb.append("SYSTEM OUT: \n");
         while ((line = input.readLine()) != null) {
-          System.out.println(line);
+          sb.append(line + "\n");
         }
       }
+      success = p.exitValue() == 0;
 
+      Files.move(outputFile, finalOutputFile);
     } catch (Exception err) {
+      sb.append(err.getMessage());
       err.printStackTrace();
     }
 
-    File finalOutputFile = new File(lipwigOutputFolder, outputFile.getName());
-    Files.move(outputFile, finalOutputFile);
+    Map<String, Object> response = new HashMap<String, Object>();
+    response.put("success", success);
 
-    return String.format("%s%s", lipwigOutputPath, finalOutputFile.getName());
+    if (success) {
+      response.put("message", String.format("%s%s", lipwigOutputPath, finalOutputFile.getName()));
+    } else {
+      response.put("message", sb.toString());
+    }
+
+    return response;
   }
 }
